862739444734b6c156da9ad66550fc17ff4b3530
hercules santos <herculessantos306@gmail.com>
2022-02-10 17:15:03 -0300

    feat: create injection via constructor and fields

--- src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
@@ -4,9 +4,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.parkingcontrol.service.ParkingSpotService;
+
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/parking-spot")
 public class ParkingSpotController {
+	
+	final ParkingSpotService parkingSpotService;
 
+	public ParkingSpotController(ParkingSpotService parkingSpotService) {
+		super();
+		this.parkingSpotService = parkingSpotService;
+	}
+
+	
+	
 }
