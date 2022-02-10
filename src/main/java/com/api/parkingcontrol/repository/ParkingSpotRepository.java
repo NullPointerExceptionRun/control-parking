29dde4c0c54082a6f0e6d012f2412ea6146ebf79
hercules santos <herculessantos306@gmail.com>
2022-02-10 17:05:19 -0300

    chore: create new class service

--- src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
@@ -1,5 +1,14 @@
 package com.api.parkingcontrol.repository;
 
-public interface ParkingSpotRepository {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.parkingcontrol.model.ParkingSpotModel;
+
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
 
 }
